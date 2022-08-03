f4ea50cdff9e83ee3bb683b9d01853152427a113
thechummel <thechummel03@gmail.com>
2022-08-03 10:34:48 -0400

    plz work

--- src/components/PostPreviewList.js
+++ src/components/PostPreviewList.js
@@ -57,25 +57,24 @@ const CardImage = ({ mediaStyle, imageURL, slug }) => {
   )
 }
 
-var docString = {postBannerImg}
-var docArray = docString.split(',');
-var docNameArray = []
-var docFileArray = []
-
-for (var i=0; i<docArray.length; i++) {
-  var tempArray = docArray[i].split('|');
-  docNameArray.push(tempArray[0]);
-  docFileArray.push(tempArray[1]);
-}
-
-var docNameFilter = docNameArray.filter(function (el) {
-  return el != '';
-})
-
-docNameArray = docNameFilter;
-
 class PostPreviewList extends React.Component {
   render () {
+    var docString = {postBannerImg}
+    var docArray = docString.split(',');
+    var docNameArray = []
+    var docFileArray = []
+
+    for (var i=0; i<docArray.length; i++) {
+      var tempArray = docArray[i].split('|');
+      docNameArray.push(tempArray[0]);
+      docFileArray.push(tempArray[1]);
+    }
+
+    var docNameFilter = docNameArray.filter(function (el) {
+      return el != '';
+    })
+
+    docNameArray = docNameFilter;
     const {
       classes,
       title,
