ff178ed0aefa24e9f25370c760d527d763c43f4f
thechummel <thechummel03@gmail.com>
2022-08-10 09:31:49 -0400

    fix

--- src/views/PostDetail.js
+++ src/views/PostDetail.js
@@ -112,11 +112,11 @@ const RenderPost = ({ data, classes, ...props }) => {
           src={post.featuredImage.sourceUrl}
         />
       )}
-      {post.categories.edges[0].node.name != 'Full-time employment' || post.categories.edges[0].node.name != 'Part-time employment' }
+      {(post.categories.edges[0].node.name != 'Full-time employment' || post.categories.edges[0].node.name != 'Part-time employment') &&
       <Typography type='caption' variant='h6' className={classes.categoryColor}>
         {post.categories.edges[0].node.name}
       </Typography>
-
+      }
 
       <Typography type='h5' className={classes.titleColor} variant='h4'>
         <SanitizedHTML html={post.title} />
