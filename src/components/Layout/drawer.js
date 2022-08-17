d0ecbc0af463b71f389e2e039dde9390e2910153
thechummel <thechummel03@gmail.com>
2022-08-17 11:18:40 -0400

    asdf

--- src/components/Layout/drawer.js
+++ src/components/Layout/drawer.js
@@ -203,7 +203,7 @@ class SideComponent extends Component {
     return (
       <div style={this.props.style}>
         <Hidden mdUp implementation='css'>
-        {/*<ClickAwayListener onClickAway={this.props.handleDrawerToggle}>*/}
+        <ClickAwayListener onClickAway={this.props.handleDrawerToggle}>
           <Drawer
             variant='temporary'
             open={this.props.open}
@@ -218,10 +218,10 @@ class SideComponent extends Component {
           >
             {drawer}
           </Drawer>
-          {/*</ClickAwayListener>*/}
+          </ClickAwayListener>
         </Hidden>
         <Hidden smDown implementation='css'>
-        <ClickAwayListener onClickAway={this.props.handleDrawerToggle}>
+        {/*<ClickAwayListener onClickAway={this.props.handleDrawerToggle}>*/}
           <Drawer
             variant='persistent'
             open
@@ -231,7 +231,7 @@ class SideComponent extends Component {
           >
             {drawer}
           </Drawer>
-          </ClickAwayListener>
+          {/*</ClickAwayListener>*/}
         </Hidden>
       </div>
     )
