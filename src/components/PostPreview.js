303978a49b7d9ba0ce11f2b7eeb40f90528f4540
thechummel <thechummel03@gmail.com>
2022-08-17 11:33:48 -0400

    tfgh

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -110,6 +110,7 @@ class PostPreview extends React.Component {
 
     var catArray = [];
     var catArrayLim = '';
+    console.log(categoriesList);
     
     if (categoriesList.length > 3) {
       catArrayLim = 3;
