b58173d3e294c659328f201db913693cd362e193
thechummel <thechummel03@gmail.com>
2022-08-18 13:50:09 -0400

    adding announcement post type

---
+++ src/components/AnnouncementPreviewList.js
@@ -0,0 +1,219 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import { blue, grey } from '@material-ui/core/colors'
+import Divider from '@material-ui/core/Divider';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import AttachmentIcon from '@material-ui/icons/Attachment';
+import RoomIcon from '@material-ui/icons/Room';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import { Link } from 'react-router-dom'
+import SanitizedHTML from 'react-sanitized-html'
+
+const styles = {
+  card: {
+    marginBottom: 15
+  },
+  titleColor: {
+    color: '#21412a'
+  },
+  dateColor: {
+    color: grey[500]
+  },
+  categoryColor: {
+    color: '#998643',
+    textDecoration: 'uppercase'
+  },
+  media: {
+    height: 'auto',
+    width: '100%'
+  },
+  link: {
+    color: 'inherit',
+    textDecoration: 'none'
+  },
+  printVAtt: {
+    width: 'auto',
+    fontSize: '18px'
+  },
+  jobTitleStyle: {
+    color: '#000000',
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '5px'
+  },
+  iconStyle: {
+    marginRight: '2px'
+  },
+  divider: {
+    marginTop: '2em',
+    marginBottom: '1em'
+  },
+  attachmentStyle: {
+    display: 'flex',
+    marginBottom: '8px',
+    width: 'fit-content'
+  }
+}
+
+const CardImage = ({ mediaStyle, imageURL, slug }) => {
+  if (!imageURL) return ''
+
+  return (
+    <Link to={`/announcement/${slug}`}>
+      <CardMedia className={mediaStyle} component='img' image={imageURL} />
+    </Link>
+  )
+}
+
+class AnnouncementPreviewList extends React.Component {
+  render () {
+
+    const {
+      classes,
+      title,
+      imageURL,
+      date,
+      slug,
+      style,
+      category,
+      categoriesList,
+      view,
+    } = this.props
+    
+
+
+    var catArray = [];
+    var catArrayLim = '';
+
+    
+    if (categoriesList.length > 3) {
+      catArrayLim = 3;
+    } else {
+      catArrayLim = categoriesList.length;
+    }
+    for (var j=0; j<catArrayLim; j++) {
+      if (categoriesList[j].node.name.toLowerCase() != 'full-time employment' &&
+      categoriesList[j].node.name.toLowerCase() != 'part-time employment' &&
+      categoriesList[j].node.name.toLowerCase() != 'internships') {
+        catArray.push(categoriesList[j].node.name)
+      }
+    }
+    var catString = catArray.join(' \u2022 ');
+
+    const postDate = new Date(date).toLocaleDateString()
+    return (
+      <div>
+        {view === 'card' ? (
+          <CardView
+            classes={classes}
+            title={title}
+            imageURL={imageURL}
+            postDate={postDate}
+            slug={slug}
+            style={style}
+            category={category}
+            categoriesList={categoriesList}
+            content={this.props.content}
+            docString={docString}
+            docNameArray={docNameArray}
+            docFileArray={docFileArray}
+            catString={catString}
+          />
+        ) : (
+          <PlainView
+            classes={classes}
+            title={title}
+            content={this.props.content}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+
+const CardView = ({
+  classes,
+  title,
+  slug,
+  imageURL,
+  postDate,
+  id,
+  style,
+  category,
+  categoriesList,
+  content,
+  docString,
+  docNameArray,
+  docFileArray,
+  catString
+}) => (
+  <div>
+    <Card className={classes.card} style={style}>
+      <CardImage mediaStyle={classes.media} imageURL={imageURL} slug={slug} />
+      <CardContent>
+        <Typography variant='button' className={classes.categoryColor}>
+          {catString}
+        </Typography>
+        <Typography className={classes.titleColor} variant='h6'>
+          <Link className={classes.link} to={`/announcement/${slug}`}>
+            <SanitizedHTML html={title} />
+          </Link>
+        </Typography>
+        <Typography className={classes.dateColor} variant='overline'>
+          {postDate}
+        </Typography>
+        {jobTitleInput == 0 ? <Typography></Typography> : 
+        <Typography paragraph='true' className={classes.jobTitleStyle} variant='overline'>
+          <AssignmentIcon className={classes.iconStyle} /> {jobTitleInput}
+        </Typography>
+        }
+        {jobLocationInput == 0 ? <Typography></Typography> : 
+        <Typography paragraph='true' className={classes.jobTitleStyle} variant='overline'>
+          <RoomIcon className={classes.iconStyle} /> {jobLocationInput}
+        </Typography>
+        }
+        {jobWorkdayInput == 0 ? <Typography></Typography> : 
+        <Typography paragraph='true' className={classes.jobTitleStyle} variant='overline'>
+          <AccessTimeIcon className={classes.iconStyle} /> {jobWorkdayInput}
+        </Typography>
+        }
+        <Typography component='div' variant='body2' gutterBottom>
+          <div className='list-view-single-announcement'>
+            <Typography
+              style={{ display: 'inline' }}
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
+          </div>
+        </Typography>
+
+        {docString == '' ? <Typography></Typography> : 
+        
+      <Typography>
+        <Divider className={classes.divider} />
+      {docNameArray.map(function(name, index){
+        return <a className={classes.attachmentStyle} key={index} href={docFileArray[index]} target="_blank" rel="noopener"><AttachmentIcon /> {name}</a>
+      })}
+      </Typography>
+      }
+
+      </CardContent>
+    </Card>
+  </div>
+)
+
+const PlainView = ({ classes, title, id, content }) => (
+  <div style={{ fontFamily: 'serif !important' }}>
+    <h1>
+      <u>{title}</u>
+    </h1>
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+    <hr />
+  </div>
+)
+
+export default withStyles(styles)(AnnouncementPreviewList)
---
+++ src/components/GridTypes/AnnouncementView.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Loader from '../Loader'
+import AnnouncementPreviewList from '../AnnouncementPreviewList'
+
+class ListView extends React.Component {
+  state = {
+    loadingFinished: false
+  }
+
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleOnScroll)
+    if (this.props.loadDone) this.setState({ loadingFinished: true })
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleOnScroll)
+  }
+
+  handleOnScroll = () => {
+    // http://stackoverflow.com/questions/9439725/javascript-how-to-detect-if-browser-window-is-scrolled-to-bottom
+    var scrollTop =
+      (document.documentElement && document.documentElement.scrollTop) ||
+      document.body.scrollTop
+    var scrollHeight =
+      (document.documentElement && document.documentElement.scrollHeight) ||
+      document.body.scrollHeight
+    var clientHeight =
+      document.documentElement.clientHeight || window.innerHeight
+    var scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight
+    if (scrolledToBottom) {
+      if (
+        this.props.announcements &&
+        this.props.announcements.pageInfo &&
+        this.props.announcements.pageInfo.hasNextPage
+      ) {
+        this.props.onLoadMore()
+      }
+    }
+  }
+
+  render () {
+    if (!this.props.announcements && this.props.loading) return <Loader />
+
+    return (
+      <>
+        <Grid container justify='center'>
+          <Grid item xs={12} sm={12} md={8}>
+            {this.props.announcements.edges.map(announcements => (
+              <AnnouncementPreviewList
+                view='card'
+                key={announcements.node.slug}
+                slug={announcements.node.slug}
+                date={announcements.node.date}
+                imageURL={
+                  announcements.node.featuredImage && announcements.node.featuredImage.sourceUrl
+                }
+                title={announcements.node.title}
+                category={announcements.node.categories.edges[0].node.name}
+                categoriesList={poannouncementsst.node.categories.edges}
+                content={announcements.node.content}
+              />
+            ))}
+          </Grid>
+        </Grid>
+        {this.props.loading && <Loader />}
+      </>
+    )
+  }
+}
+
+export default ListView
